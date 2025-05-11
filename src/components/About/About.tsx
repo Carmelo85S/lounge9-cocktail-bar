60dec8b329006de672e7caabf7e734fb016e266e
Carmelo85S <carmelo.salis85@gmail.com>
2025-05-11 18:23:47 +0200

    about section>

---
+++ src/components/About/About.tsx
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <section>
+        <section>
+            <h2>Welcome to Lounge<span>9</span></h2>
+            <p>Where every moment is crafted to perfection</p>
+        </section>
+        <section>
+            <h3>About Us</h3>
+            <p>
+                Nestled within the elegant Grand Plaza Hotel, Lounge<span>9</span> Hour offers a sophisticated 
+                yet relaxed atmosphere for both hotel guests and locals alike. Our bar combines 
+                classic charm with contemporary style, creating the perfect setting for any occasion.
+            </p>   
+            <p>
+                Whether you're unwinding after a day of meetings with our signature cocktails, enjoying artisanal 
+                pizzas with friends, or experiencing one of our vibrant DJ nights, 
+                Lounge<span>9</span> Hour promises an unforgettable experience.
+            </p>   
+        </section>
+    </section>
+  )
+}
+
+export default About
