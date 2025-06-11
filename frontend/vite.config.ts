04f523c45918e4f600dffbd5ec75eaf0a6167f9c
Carmelo85S <carmelo.salis85@gmail.com>
2025-06-11 06:55:50 +0200

    change in vite config for deploy in vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lounge9-cocktail-bar/',
+  base: '/',
   build: { chunkSizeWarningLimit: 1600, },
 })
